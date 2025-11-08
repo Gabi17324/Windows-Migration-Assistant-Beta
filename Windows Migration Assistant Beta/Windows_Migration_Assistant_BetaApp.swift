2dd8b3a8d7c0c751c10b833380e87b3eb934981b
Gabi <vladugabi2012@gmail.com>
2025-11-07 23:30:26 -0800

    Initial Commit

---
+++ Windows Migration Assistant Beta/Windows_Migration_Assistant_BetaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Windows_Migration_Assistant_BetaApp.swift
+//  Windows Migration Assistant Beta
+//
+//  Created by Gabi on 07.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct Windows_Migration_Assistant_BetaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Windows Migration Assistant BetaUITests/Windows_Migration_Assistant_BetaUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Windows_Migration_Assistant_BetaUITestsLaunchTests.swift
+//  Windows Migration Assistant BetaUITests
+//
+//  Created by Gabi on 07.11.2025.
+//
+
+import XCTest
+
+final class Windows_Migration_Assistant_BetaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
